90239fa9d4222b550c8a672e216e6f6335824414
Md. Adib Zawad <158603527+adib-zawad@users.noreply.github.com>
2026-04-18 19:45:08 +0600

    Add files via upload

---
+++ count_nodes_linked_list.cpp
@@ -0,0 +1,36 @@
+// Problem: Count number of nodes in a linked list
+// Approach: Traverse the list and increment counter
+// Language: C++
+
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+// Function to count nodes
+int countNodes(Node* head) {
+    int count = 0;
+    Node* current = head;
+
+    while (current != NULL) {
+        count++;
+        current = current->next;
+    }
+
+    return count;
+}
+
+int main() {
+    // Creating linked list: 10 -> 20 -> 30 -> 40
+    Node* head = new Node{10, NULL};
+    head->next = new Node{20, NULL};
+    head->next->next = new Node{30, NULL};
+    head->next->next->next = new Node{40, NULL};
+
+    cout << "Number of nodes: " << countNodes(head) << endl;
+
+    return 0;
+}
