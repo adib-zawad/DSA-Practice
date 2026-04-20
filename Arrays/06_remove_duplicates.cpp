c78b688784addfc16f3ebe3ce2e06ba6f9d96b55
Md. Adib Zawad <158603527+adib-zawad@users.noreply.github.com>
2026-04-20 20:10:42 +0600

    Create 06_remove_duplicates.cpp

---
+++ Arrays/06_remove_duplicates.cpp
@@ -0,0 +1,22 @@
+// Remove duplicates from sorted array
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 1, 2, 2, 3, 3};
+    int n = 6;
+
+    int j = 0;
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] != arr[j]) {
+            j++;
+            arr[j] = arr[i];
+        }
+    }
+
+    for (int i = 0; i <= j; i++) {
+        cout << arr[i] << " ";
+    }
+}
