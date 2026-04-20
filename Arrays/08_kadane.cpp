86578963f4bc4be81713e1439c59bb4b34284993
Md. Adib Zawad <158603527+adib-zawad@users.noreply.github.com>
2026-04-20 20:20:02 +0600

    Create 08_kadane.cpp

---
+++ Arrays/08_kadane.cpp
@@ -0,0 +1,19 @@
+// Maximum subarray sum (Kadane's Algorithm)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n = 9;
+
+    int maxSum = arr[0];
+    int currentSum = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        currentSum = max(arr[i], currentSum + arr[i]);
+        maxSum = max(maxSum, currentSum);
+    }
+
+    cout << "Max Sum: " << maxSum << endl;
+}
